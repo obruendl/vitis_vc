773ad49be494366fb2a74d0a977274d3a17c182b
Oliver Bründler <oli.bruendler@gmx.ch>
2023-11-02 06:58:41 +0100

    update

---
+++ ws/regenerate.tcl
@@ -0,0 +1,24 @@
+puts "*** Regenerate all BSPs ***"
+
+#Remark: Tested with a single platform only!
+
+#Read all platforms
+foreach spr [glob **/*.spr] {
+	platform read $spr
+}
+
+#Leave untouched
+set platforms [dict keys [platform list -dict]]
+foreach platform $platforms {
+	puts "> Platform: $platform"
+	platform generate $platform
+	platform active $platform
+	set domains [dict keys [domain list -dict]]
+	foreach domain $domains {
+		puts " > Domain: $domain"
+		domain active $domain
+		::scw::regenerate_psinit 
+		bsp reload
+		bsp regenerate
+	}
+}
